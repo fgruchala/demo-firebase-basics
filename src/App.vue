89d800d0738c507dd9f1924d63a9072182d2b43f
François GRUCHALA <francois.gruchala@gmail.com>
2019-01-09 23:12:54 +0100

    :sparkles: add authentification part

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,11 @@
   <md-app id="app">
     <md-app-toolbar md-elevation="1">
       <div class="md-toolbar-row">
-        <div class="md-title">
+        <div class="md-toolbar-section-start">
           <img alt="logo" src="./assets/logo.png">
-          <span>Basics with VueJS</span>
+        </div>
+        <div class="md-toolbar-section-end">
+          <div class="md-title">Basics</div>
         </div>
       </div>
     </md-app-toolbar>
@@ -16,6 +18,52 @@
                  md-dynamic-height>
           <md-tab md-icon="verified_user"
                   md-label="S'authentifier">
+
+            <form @submit.prevent="signIn"
+                  novalidate>
+
+              <md-field>
+                <label>Identifiant</label>
+                <md-input autofocus
+                          v-model.trim="login"/>
+              </md-field>
+              <md-field>
+                <label>Mot de passe</label>
+                <md-input type="password"
+                          v-model.trim="password"/>
+              </md-field>
+
+              <p class="actions">
+                <md-button @click="signInWithGoogle"
+                           class="md-icon-button">
+                  <img src="./assets/logo-btn-google.svg"/>
+                </md-button>
+                <span class="space"></span>
+                <md-button @click="createNewAccount">Créer un nouveau compte</md-button>
+                <md-button class="md-primary"
+                           type="submit">
+                  Se connecter
+                </md-button>
+              </p>
+            </form>
+
+            <p class="results">
+              <md-empty-state :md-size="350"
+                              class="is-logged"
+                              md-icon="lock_open"
+                              md-label="Authentifié"
+                              md-rounded
+                              v-if="!isLogged">
+                <md-button @click="signOut">Se déconnecter</md-button>
+              </md-empty-state>
+
+              <md-empty-state :md-size="350"
+                              class="is-not-logged"
+                              md-icon="lock"
+                              md-label="Non authentifié"
+                              md-rounded
+                              v-else/>
+            </p>
 
           </md-tab>
 
@@ -37,6 +85,30 @@
 <script>
 export default {
   name: 'app',
+  data() {
+    return {
+      isLogged: false,
+      login: null,
+      password: null,
+    };
+  },
+  created() {
+    // TODO check if user is logged
+  },
+  methods: {
+    signIn() {
+      // TODO authenticate with login/password
+    },
+    signInWithGoogle() {
+      // TODO authenticate with Google SSO
+    },
+    signOut() {
+      // TODO sign out
+    },
+    createNewAccount() {
+      // TODO create new account with login/password
+    },
+  },
 };
 </script>
 
@@ -44,16 +116,7 @@ export default {
   #app {
     height: 100%;
 
-    .md-title {
-      display: grid;
-      grid-column-gap: 20px;
-
-      span,
-      img {
-        grid-row: 1;
-        align-self: center;
-      }
-
+    .md-toolbar {
       img {
         height: 30px;
         max-width: 100%;
@@ -63,6 +126,37 @@ export default {
     .content {
       width: 600px;
       margin: auto;
+
+      .actions {
+        display: flex;
+        align-items: center;
+
+        .space {
+          flex: 1;
+        }
+      }
+
+      .results {
+        text-align: center;
+
+        .is-logged {
+          background-color: rgba(76, 175, 80, 0.06);
+
+          &,
+          .md-icon {
+            color: rgb(76, 175, 80);
+          }
+        }
+
+        .is-not-logged {
+          background-color: rgba(244, 67, 54, 0.06);
+
+          &,
+          .md-icon {
+            color: rgb(244, 67, 54);
+          }
+        }
+      }
     }
   }
 </style>
