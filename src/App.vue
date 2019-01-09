51d3db2cd39f5fc4dcaf18383bd7184d50a2b1d0
François GRUCHALA <francois.gruchala@gmail.com>
2019-01-09 13:22:46 +0100

    :tada: initial import

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <img alt="logo" src="./assets/logo.png">
+    <HelloWorld msg="Basics with VueJS"/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue';
+
+export default {
+  name: 'app',
+  components: {
+    HelloWorld,
+  },
+};
+</script>
+
+<style lang="scss">
+  @import url('https://fonts.googleapis.com/css?family=Roboto');
+
+  #app {
+    text-align: center;
+    color: #2C3E50;
+    margin-top: 60px;
+    font-family: 'Roboto';
+
+    img {
+      height: 200px;
+      max-width: 100%;
+    }
+  }
+</style>
